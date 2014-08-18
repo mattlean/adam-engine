c1034b420fd09a78b123ff854646d73f52061196
Isaac Lean <isaaclean@creatibyte.com>
2014-08-18 07:37:45 -0700

    Started working on matching algorithm

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ window.onload = function() {
 	var canvasHeight = canvas.height;
 	var game_loop;
 	var cellSize = 40; //determines cell size. cell is always a square.
+	var boardSize = 8; //determines board size. at the moment the board is always square.
 	var board = [];
 	var score;
 
@@ -14,8 +15,12 @@ window.onload = function() {
 
 	/* Start Game */
 	function init() {
-		for(var i = 0; i < 10; ++i) {
-			board.push([generateJewel(), generateJewel(), generateJewel(), generateJewel(), generateJewel(), generateJewel(), generateJewel(), generateJewel()]);
+		for(var i = 0; i < boardSize; ++i) {
+			var temp = [];
+			for(var j = 0; j < boardSize; ++j) {
+				temp.push(generateJewel());
+			}
+			board.push(temp);
 		}
 		console.log(board);
 		score = 0;
@@ -38,14 +43,40 @@ window.onload = function() {
 		ctx.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
 	}
 
+	function match(board) {
+		var count = 0;
+		var prevVal = -1;
+		for(var j = 0; j < boardSize; ++j) {
+			for(var i = 0; i < boardSize; ++i) {
+				var currVal = board[j][i];
+				//console.log(currVal);
+				if(currVal === prevVal) {
+					++count;
+					//console.log(count);
+				} else {
+					if(count >= 2) {
+						console.log('x:' + i + ', ' + 'y:' + j + ', ' + 'count:' + count);
+					}
+					count = 0;
+					//console.log(count);
+				}
+				prevVal = currVal;
+			}
+			if(count >= 2) {
+				console.log('x:' + i + ', ' + 'y:' + j + ', ' + 'count:' + count);
+			}
+			count = 0;
+		}
+	}
+
 	function draw() {
 		//draw the background to clear previous frame
 		ctx.fillStyle = '#fff';
 		ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-		for(var i = 0; i < board[0].length; ++i) {
-			for(var j = 0; j < 8; ++j) {
-				drawJewel(j, i, board[i][j]);
+		for(var i = 0; i < boardSize; ++i) {
+			for(var j = 0; j < boardSize; ++j) {
+				drawJewel(i, j, board[j][i]);
 			}
 		}
 
@@ -53,6 +84,8 @@ window.onload = function() {
 		ctx.fillStyle = '#000';
 		ctx.fillText(score_text, canvasWidth - 155, 15);
 	}
+
+	match(board);
 
 	/* Input */
 	window.onmousedown = function(evt) {
