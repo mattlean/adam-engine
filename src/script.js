6177f58d67e7b5c0b433fad4f8c60ac529b77334
Isaac Lean <isaaclean@creatibyte.com>
2014-08-18 04:45:28 -0700

    The snake can move now!

--- src/script.js
+++ src/script.js
@@ -1,17 +1,18 @@
 window.onload = function() {
 	var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var canvasWidth = canvas.width;
-    var canvasHeight = canvas.height;
-
-    ctx.fillStyle = '#fff';
-	ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+	var ctx = canvas.getContext('2d');
+	var canvasWidth = canvas.width;
+	var canvasHeight = canvas.height;
+	var cellWidth = 10;
+	var direction = 'right'; //default direction
 
 	var snake_array;
 
 	function create_snake() {
 		var length = 5;
 		snake_array = [];
+		//builds snake_array from the head (right-most snake cell when the game starts)
+		//to tail (left-most snake cell when the game starts)
 		for(var i = length-1; i >= 0; --i) {
 			snake_array.push({x: i, y:0});
 		}
@@ -20,14 +21,61 @@ window.onload = function() {
 	create_snake();
 
 	function draw() {
+		//draw the background to clear previous frame
+		ctx.fillStyle = '#fff';
+		ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+		//move the snake
+		//get snake head's current x and y pos
+		var newX = snake_array[0].x;
+		var newY = snake_array[0].y;
+		//manages direction of snake
+		if(direction === 'right') {
+			++newX;
+		} else if(direction === 'left') {
+			--newX;
+		} else if(direction === 'up') {
+			--newY;
+		} else if(direction === 'down') {
+			++newY;
+		}
+
+		//If the snake hits a wall restart the game
+		if(newX === -1 || newX === canvasWidth/cellWidth || newY === -1 || newY === canvasHeight/cellWidth) {
+			//Restart the game here
+			return;
+		}
+
+		var tail = snake_array.pop(); //pop the tail of the snake
+		tail.x = newX;
+		tail.y = newY;
+		snake_array.unshift(tail); //add the new head of the snake in front of the curr head
+		
+		//draw the snake
 		for(var i = 0; i < snake_array.length; ++i) {
-			var c = snake_array[i];
+			var snakeCell = snake_array[i];
 			ctx.fillStyle = '#00ff00';
-			ctx.fillRect(c.x*10, c.y*10, 10, 10);
+			ctx.fillRect(snakeCell.x * cellWidth, snakeCell.y * cellWidth, cellWidth, cellWidth);
 			ctx.strokeStyle = '#fff';
-			ctx.strokeRect(c.x * 10, c.y * 10, 10, 10);
+			ctx.strokeRect(snakeCell.x * cellWidth, snakeCell.y * cellWidth, cellWidth, cellWidth);
 		}
 	}
 
+	/* Input */
+	window.onkeydown = function(evt) {
+		var key = evt.which;
+		if((key === 37) && (direction !== 'right')) {
+			direction = 'left';
+		} else if((key === 39) && (direction !== 'left')) {
+			direction = 'right';
+		} else if((key === 38) && (direction !== 'down')) {
+			direction = 'up';
+		} else if((key === 40) && (direction !== 'up')) {
+			direction = 'down';
+		}
+	};
+
 	draw();
+
+	var game_loop = setInterval(draw, 60);
 };
