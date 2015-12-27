456fefcbe159d8533e731e3cb7a17ede7cd2c5ed
Matthew Isaac Lean <lean@miles.co>
2015-12-26 21:03:33 -0800

    Experiments with Object.create

--- src/engine-refactor/adamengine/adamengine-dev.js
+++ src/engine-refactor/adamengine/adamengine-dev.js
@@ -51,99 +51,185 @@ body.appendChild(canvas);
 // requestAnimationFrame(animate);
 
 /* Atlas test */
-function parseAtlasDefinition(atlasJSON) {
-	var parsed = JSON.parse(atlasJSON);
+// function parseAtlasDefinition(atlasJSON) {
+// 	var parsed = JSON.parse(atlasJSON);
 
-	/*for(var key in parsed.frames) {
-		var sprite = parsed.frames[key];
-		// define center of sprite as offset
-		var cx = -sprite.frame.w * 0.5;
-		var cy = -sprite.frame.h * 0.5;
+// 	/*for(var key in parsed.frames) {
+// 		var sprite = parsed.frames[key];
+// 		// define center of sprite as offset
+// 		var cx = -sprite.frame.w * 0.5;
+// 		var cy = -sprite.frame.h * 0.5;
 
-		// define the sprite for this sheet
-		//this.defSprite(key, sprite.frame.x, sprite.frame.y, sprite.frame.w, sprite.frame.h, cx, cy);
-	}*/
-}
+// 		// define the sprite for this sheet
+// 		//this.defSprite(key, sprite.frame.x, sprite.frame.y, sprite.frame.w, sprite.frame.h, cx, cy);
+// 	}*/
+// }
 
-function main() {
-	function onClick(e) {
-		var canvasOffset = canvas.getBoundingClientRect();
-		console.log((e.clientX - canvasOffset.left) + ', ' + (e.clientY - canvasOffset.top));
-	}
+// function main() {
+// 	function onClick(e) {
+// 		var canvasOffset = canvas.getBoundingClientRect();
+// 		console.log((e.clientX - canvasOffset.left) + ', ' + (e.clientY - canvasOffset.top));
+// 	}
 
-	var atlasJson = null;
+// 	var atlasJson = null;
 
-	var image = new Image();
-	image.src = 'img/atlas.png';
-	image.onload = function() {
-		// attempt dl of atlas JSON
-		var xhrAtlasJson = new XMLHttpRequest();
-		xhrAtlasJson.responseType = 'json';
-		xhrAtlasJson.open('GET', '/img/atlas.json', true);
-		xhrAtlasJson.responseType = 'json';
-		xhrAtlasJson.onload = function() {
-			// store atlas JSON
-			atlasJson = this.response;
+// 	var image = new Image();
+// 	image.src = 'img/atlas.png';
+// 	image.onload = function() {
+// 		// attempt dl of atlas JSON
+// 		var xhrAtlasJson = new XMLHttpRequest();
+// 		xhrAtlasJson.responseType = 'json';
+// 		xhrAtlasJson.open('GET', '/img/atlas.json', true);
+// 		xhrAtlasJson.responseType = 'json';
+// 		xhrAtlasJson.onload = function() {
+// 			// store atlas JSON
+// 			atlasJson = this.response;
 
-			ctx.drawImage(
-				image,
-				atlasJson.frames['BHQ2.png'].frame.x,
-				atlasJson.frames['BHQ2.png'].frame.y,
-				atlasJson.frames['BHQ2.png'].sourceSize.w,
-				atlasJson.frames['BHQ2.png'].sourceSize.h,
-				10, 10, 70, 70
-			);
+// 			ctx.drawImage(
+// 				image,
+// 				atlasJson.frames['BHQ2.png'].frame.x,
+// 				atlasJson.frames['BHQ2.png'].frame.y,
+// 				atlasJson.frames['BHQ2.png'].sourceSize.w,
+// 				atlasJson.frames['BHQ2.png'].sourceSize.h,
+// 				10, 10, 70, 70
+// 			);
 
-			ctx.drawImage(
-				image,
-				atlasJson.frames['FAM2.png'].frame.x,
-				atlasJson.frames['FAM2.png'].frame.y,
-				atlasJson.frames['FAM2.png'].sourceSize.w,
-				atlasJson.frames['FAM2.png'].sourceSize.h,
-				100, 100, 70, 70
-			);
+// 			ctx.drawImage(
+// 				image,
+// 				atlasJson.frames['FAM2.png'].frame.x,
+// 				atlasJson.frames['FAM2.png'].frame.y,
+// 				atlasJson.frames['FAM2.png'].sourceSize.w,
+// 				atlasJson.frames['FAM2.png'].sourceSize.h,
+// 				100, 100, 70, 70
+// 			);
 
-			console.log(canvas);
-			console.log(onClick);
-			canvas.addEventListener('click', onClick);
-		};
-		xhrAtlasJson.send();
-	};
-}
+// 			// console.log(canvas);
+// 			// console.log(onClick);
+// 			canvas.addEventListener('click', onClick);
+// 		};
+// 		xhrAtlasJson.send();
+// 	};
+// }
 
-main();
+// main();
 
-var AdamEngine = function() {
-	this.gameObjs = {};
-}
+var AdamEngine = {
+	worldObjs: {},
+	storeObjs: {},
 
-AdamEngine.prototype.createGameObj = function(gameObjName) {
-	this.gameObjs[gameObjName] = {
+	/* Classes */
+	gameObj: {
+		type: null,
 		state: {},
 		setup: function() {},
 		update: function() {}
+	},
+
+	// Initialize the game engine so everything can start working
+	construct: function () {
+		// world objs inherit from game obj
+		this.worldObj = Object.create(this.gameObj);
+		this.worldObj.type = 'worldObj';
+		this.worldObj.state = {
+			worldObjType: null,
+			pos: {x: 0, y: 0},
+			size: {w: 0, h: 0},
+			image: null,
+			color: null,
+			stroke: null
+		};
+		this.worldObj.render= function() {};
+
+		// store objs inherit from game obj
+		this.storeObj = Object.create(this.gameObj);
+		this.storeObj.type = 'storeObj';
+	},
+
+	/* Game Object Creators/Destroyers */
+	createWorldObj: function(worldObjName) {
+		this.worldObjs[worldObjName] = Object.create(this.worldObj);
+		return this.worldObjs[worldObjName];
+	},
+
+	deleteWorldObj: function(worldObjName) {
+		delete this.worldObjs[worldObjName];
+	},
+
+	createStoreObj: function(storeObjName) {
+		this.storeObjs[storeObjName] = Object.create(this.storeObj);
+		return this.storeObjs[storeObjName];
+	},
+
+	deleteStoreObj: function(storeObjName) {
+		this.storeObjs[storeObjName] = Object.create(this.storeObj);
+		return this.storeObjs[storeObjName];
+	},
+
+	/* Game Loop */
+	start: function() {
+		// run setup for all game objs
+		for(var storeObjName in this.storeObjs) {
+			this.storeObjs[storeObjName].setup();
+		}
+
+		for(var worldObjName in this.worldObjs) {
+			this.worldObjs[worldObjName].setup();
+		}
+
+		AdamEngine.update(); // start game loop
+	},
+
+	update: function(cb) {
+		for(var worldObjName in this.worldObjs) {
+			this.worldObjs[worldObjName].update();
+		}
+
+		this.render();
+	},
+
+	render: function() {
+		// render all pos x & y of all world objs
+		for(var worldObjName in this.worldObjs) {
+			var worldObj = this.worldObjs[worldObjName];
+			if(worldObj.type === 'worldObj') {
+				if(worldObj.state.worldObjType === 'rect') {
+					ctx.fillStyle = worldObj.state.color;
+					ctx.fillRect(worldObj.state.pos.x, worldObj.state.pos.y, worldObj.state.size.w, worldObj.state.size.h);
+
+					if(worldObj.state.stroke !== null) {
+						ctx.strokeStyle = worldObj.state.stroke.color;
+						ctx.strokeRect(worldObj.state.stroke.x, worldObj.state.stroke.y, worldObj.state.stroke.w, worldObj.state.stroke.h);
+					}
+				}
+			}
+		}
+	}
+}
+
+AdamEngine.construct();
+
+var block = AdamEngine.createWorldObj('block');
+
+block.setup = function() {
+	this.state.pos = {
+		x: 10,
+		y: 10
 	};
 
-	return this.gameObjs[gameObjName];
+	this.state.size = {
+		w: 10,
+		h: 10
+	};
+
+	this.state.worldObjType = 'rect';
+	this.state.color = '#00FF00';
+	this.state.stroke = {x: 10, y: 10, w: 10, h: 10, color: '#FFF'};
 };
 
-AdamEngine.prototype.deleteGameObj = function(gameObjName) {
-	delete this.gameObjs[gameObjName];
+block.update = function() {
+	this.state.pos.x += 100;
 };
 
-AdamEngine.prototype.update = function() {};
-
-AdamEngine.prototype.render = function() {};
-
-var AE = new AdamEngine();
-var Grid = AE.createGameObj('Grid');
-Grid.setup = function() {
-	this.state.grid = [];
-	this.state.grid.push([])
-};
-
-AE.update();
+AdamEngine.start();
 
 
-
-// initialize all game objects first
