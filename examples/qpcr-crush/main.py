2ba14b5ef3cbca112ba9695c3d6ef08a9b998f8c
Matthew Isaac Lean <matt@mattlean.com>
2016-01-07 15:32:48 -0800

    Fix score submission bug, ignore .pyc, add logging for submissions

--- examples/qpcr-crush/main.py
+++ examples/qpcr-crush/main.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 
-import jinja2
 import json
 import logging
 import webapp2
 from google.appengine.ext import db
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-	extensions=['jinja2.ext.autoescape'],
-	autoescape=True)
 
 class LBEntry(db.Model):
 	alias = db.StringProperty(required = True)
@@ -30,6 +24,8 @@ class EndPoint(webapp2.RequestHandler):
 		alias = self.request.get('alias')
 		badgeId = self.request.get('badgeId')
 		score = int(self.request.get('score'))
+
+		logging.info('New Score Submissions\nAlias: %s\nBadge ID: %s\nScore: %i', alias, badgeId, score)
 
 		newLBEntry = LBEntry(alias=alias, badgeId=badgeId, score=score)
 		newLBEntry.put()
