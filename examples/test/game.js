599cb578610335d430f2e554b9018e562f1a7c54
Matthew Lean <matt@mattlean.com>
2017-01-14 00:17:26 -0800

    Fix test, fix scroll bug, update Bootstrap, add minified game.html, new ver 1-1

--- examples/test/game.js
+++ examples/test/game.js
@@ -1,174 +1,172 @@
-(function(){
-	var AE = new AdamEngine(canvasId);
+var AE = new AdamEngine('adam-engine');
 
-	AE.assetMan.newAtlas('tiles', '/img/atlas.png', '/img/atlas.json');
+AE.assetMan.newAtlas('tiles', '/img/atlas.png', '/img/atlas.json');
 
-	AE.inputMan.addKeyInput(37, 'LEFT');
-	AE.inputMan.addKeyInput(38, 'UP');
-	AE.inputMan.addKeyInput(39, 'RIGHT');
-	AE.inputMan.addKeyInput(40, 'DOWN');
-	AE.inputMan.addMBInput(0, 'LEFTCLICK');
-	AE.inputMan.setup();
+AE.inputMan.addKeyInput(37, 'LEFT');
+AE.inputMan.addKeyInput(38, 'UP');
+AE.inputMan.addKeyInput(39, 'RIGHT');
+AE.inputMan.addKeyInput(40, 'DOWN');
+AE.inputMan.addMBInput(0, 'LEFTCLICK');
+AE.inputMan.setup();
 
-	var test = AE.createWorldObj('test');
-	test.setup = function() {
-		this.state.pos = {
-			x: 10,
-			y: 10
-		};
-
-		this.state.size = {
-			w: 10,
-			h: 10
-		};
-
-		this.state.worldObjType = 'rect';
-		this.state.color = '#0000FF';
+var test = AE.createWorldObj('test');
+test.setup = function() {
+	this.state.pos = {
+		x: 10,
+		y: 10
 	};
 
-	test.update = function() {
+	this.state.size = {
+		w: 10,
+		h: 10
+	};
+
+	this.state.worldObjType = 'rect';
+	this.state.color = '#0000FF';
+};
+
+test.update = function() {
+	this.state.pos.x += 1;
+	this.state.pos.y += 1;
+};
+
+var test2 = AE.createWorldObj('test2');
+test2.setup = function() {
+	this.state.pos = {
+		x: 5,
+		y: 5
+	};
+
+	this.state.size = {
+		w: 20,
+		h: 20
+	};
+
+	this.state.worldObjType = 'rect';
+	this.state.color = '#FF0000';
+	this.state.zIndex = 200;
+};
+
+test2.update = function() {
+	this.state.pos.y += 2;
+}
+
+var zone = AE.createWorldObj('zone');
+zone.setup = function() {
+	this.state.pos = {
+		x: 50,
+		y: 50
+	};
+
+	this.state.size = {
+		w: 30,
+		h: 30
+	};
+
+	this.state.worldObjType = 'rect';
+	this.state.color = '#000';
+	this.state.zIndex = 9001;
+}
+
+var player = AE.createWorldObj('player');
+player.setup = function() {
+	this.state.pos = {
+		x: 10,
+		y: 10
+	};
+
+	this.state.size = {
+		w: 10,
+		h: 10
+	};
+
+	this.state.worldObjType = 'rect';
+	this.state.color = '#00FF00';
+	this.state.stroke = {
+		pos: this.state.pos,
+		size: this.state.size,
+		color: '#FFF'
+	};
+	this.state.zone = zone;
+	this.state.zIndex = 200;
+};
+
+player.update = function() {
+	if(AE.inputMan.getKeyState('LEFT')) {
+		this.state.pos.x -= 1;
+	} else if(AE.inputMan.getKeyState('RIGHT')) {
 		this.state.pos.x += 1;
+	}
+
+	if(AE.inputMan.getKeyState('UP')) {
+		this.state.pos.y -= 1;
+	} else if(AE.inputMan.getKeyState('DOWN')) {
 		this.state.pos.y += 1;
-	};
-
-	var test2 = AE.createWorldObj('test2');
-	test2.setup = function() {
-		this.state.pos = {
-			x: 5,
-			y: 5
-		};
-
-		this.state.size = {
-			w: 20,
-			h: 20
-		};
-
-		this.state.worldObjType = 'rect';
-		this.state.color = '#FF0000';
-		this.state.zIndex = 200;
-	};
-
-	test2.update = function() {
-		this.state.pos.y += 2;
 	}
 
-	var zone = AE.createWorldObj('zone');
-	zone.setup = function() {
-		this.state.pos = {
-			x: 50,
-			y: 50
-		};
-
-		this.state.size = {
-			w: 30,
-			h: 30
-		};
-
-		this.state.worldObjType = 'rect';
-		this.state.color = '#000';
-		this.state.zIndex = 9001;
+	if(AE.inputMan.getMBState('LEFTCLICK').isActive) {
+		console.log('teehee~');
 	}
 
-	var player = AE.createWorldObj('player');
-	player.setup = function() {
-		this.state.pos = {
-			x: 10,
-			y: 10
-		};
+	if(
+		(this.state.pos.x < (this.state.zone.state.pos.x + this.state.zone.state.size.w)) &&
+		((this.state.pos.x + this.state.size.w) > this.state.zone.state.pos.x) &&
+		(this.state.pos.y < (this.state.zone.state.pos.y + this.state.zone.state.size.h)) &&
+		((this.state.pos.y + this.state.size.h) > this.state.zone.state.pos.y)
+	) {
+		console.log('player collision with zone');
+	}
+};
 
-		this.state.size = {
-			w: 10,
-			h: 10
-		};
-
-		this.state.worldObjType = 'rect';
-		this.state.color = '#00FF00';
-		this.state.stroke = {
-			pos: this.state.pos,
-			size: this.state.size,
-			color: '#FFF'
-		};
-		this.state.zone = zone;
-		this.state.zIndex = 200;
+var zone2 = AE.createWorldObj('zone2');
+zone2.setup = function() {
+	this.state.pos = {
+		x: 100,
+		y: 50
 	};
 
-	player.update = function() {
-		if(AE.inputMan.getKeyState('LEFT')) {
-			this.state.pos.x -= 1;
-		} else if(AE.inputMan.getKeyState('RIGHT')) {
-			this.state.pos.x += 1;
-		}
-
-		if(AE.inputMan.getKeyState('UP')) {
-			this.state.pos.y -= 1;
-		} else if(AE.inputMan.getKeyState('DOWN')) {
-			this.state.pos.y += 1;
-		}
-
-		if(AE.inputMan.getMBState('LEFTCLICK').isActive) {
-			console.log('teehee~');
-		}
-
-		if(
-			(this.state.pos.x < (this.state.zone.state.pos.x + this.state.zone.state.size.w)) &&
-			((this.state.pos.x + this.state.size.w) > this.state.zone.state.pos.x) &&
-			(this.state.pos.y < (this.state.zone.state.pos.y + this.state.zone.state.size.h)) &&
-			((this.state.pos.y + this.state.size.h) > this.state.zone.state.pos.y)
-		) {
-			console.log('player collision with zone');
-		}
+	this.state.size = {
+		w: 40,
+		h: 40
 	};
 
-	var zone2 = AE.createWorldObj('zone2');
-	zone2.setup = function() {
-		this.state.pos = {
-			x: 100,
-			y: 50
-		};
+	this.state.worldObjType = 'rect';
+	this.state.color = '#000';
+	this.state.zIndex = 100;
+};
 
-		this.state.size = {
-			w: 40,
-			h: 40
-		};
-
-		this.state.worldObjType = 'rect';
-		this.state.color = '#000';
-		this.state.zIndex = 100;
-	};
-
-	var imgTest = AE.createWorldObj('spriteTest');
-	imgTest.setup = function() {
-		this.state.atlas = AE.assetMan.getAtlas('tiles');
-		this.state.pos = {
-			x: 50,
-			y: 50
-		}
-
-		this.state.size = {
-			w: 70,
-			h: 70
-		};
-
-		var atlas = this.state.atlas.data;
-
-		this.state.img = this.state.atlas.img;
-		this.state.imgData = {
-			sx: atlas.frames['BHQ2.png'].frame.x,
-			sy: atlas.frames['BHQ2.png'].frame.y,
-			sw: atlas.frames['BHQ2.png'].sourceSize.w,
-			sh: atlas.frames['BHQ2.png'].sourceSize.h
-		};
-
-		this.state.worldObjType = 'img';
-	};
-
-	imgTest.update = function() {
-		if(AE.inputMan.getKeyState('UP')) {
-			this.state.pos.y -= 10;
-		} else if(AE.inputMan.getKeyState('DOWN')) {
-			this.state.pos.y += 10;
-		}
+var imgTest = AE.createWorldObj('spriteTest');
+imgTest.setup = function() {
+	this.state.atlas = AE.assetMan.getAtlas('tiles');
+	this.state.pos = {
+		x: 50,
+		y: 50
 	}
 
-	AE.start();
-}());
+	this.state.size = {
+		w: 70,
+		h: 70
+	};
+
+	var atlas = this.state.atlas.data;
+
+	this.state.img = this.state.atlas.img;
+	this.state.imgData = {
+		sx: atlas.frames['BHQ2.png'].frame.x,
+		sy: atlas.frames['BHQ2.png'].frame.y,
+		sw: atlas.frames['BHQ2.png'].sourceSize.w,
+		sh: atlas.frames['BHQ2.png'].sourceSize.h
+	};
+
+	this.state.worldObjType = 'img';
+};
+
+imgTest.update = function() {
+	if(AE.inputMan.getKeyState('UP')) {
+		this.state.pos.y -= 10;
+	} else if(AE.inputMan.getKeyState('DOWN')) {
+		this.state.pos.y += 10;
+	}
+}
+
+AE.start();
